e2dbb2cd296d6a1b77f77894ec657a479667c108
Abbas <tundeeey@gmail.com>
2025-07-03 15:19:43 +0100

    successfully created my landing page which is the index.html and styling, i also created a base html which was inherited by my index.html and will also be inherited by the other html that i will be adding

---
+++ app.py
@@ -0,0 +1,14 @@
+import json
+from flask import Flask, render_template, url_for, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
