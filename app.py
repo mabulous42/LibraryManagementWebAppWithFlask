d5be92e054d57ed76b1a61745a618ce2191f4644
Abbas <tundeeey@gmail.com>
2025-07-29 16:53:12 +0100

    changed the debugger port again because render is unable to detect port while deploying my project on render

--- app.py
+++ app.py
@@ -454,9 +454,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    # For development
-    app.run(debug=True)
-else:
-    # For production (Render)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
