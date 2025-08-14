df2ea69b9c01bb2a886c4f21da6a6d86da30da37
Abbas <tundeeey@gmail.com>
2025-08-14 14:38:03 +0100

    user can now tuggle password to see and hide their password

--- static/project.js
+++ static/project.js
@@ -1,10 +1,22 @@
 let password = document.getElementById('user-password')
+let showpassword = document.getElementById("show-password")
+let hidepassword = document.getElementById("hide-password")
 
-function showAndHidePassword() {
+hidepassword.style.display = 'none'
+
+
+function showPassword() {
     if (password.type == 'password') {
-        password.type = 'text'   
-        
-    } else {
-        password.type = 'password'   
-    }
+        password.type = 'text' 
+        showpassword.style.display = 'none'         
+        hidepassword.style.display = 'block'         
+    } 
+}
+
+function hidePassword() {
+    if (password.type == 'text') {
+        password.type = 'password' 
+        showpassword.style.display = 'block'         
+        hidepassword.style.display = 'none'         
+    } 
 }
